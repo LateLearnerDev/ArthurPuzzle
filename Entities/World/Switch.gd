389c122bb62f0e7180850465627c3dc5837a989f
Ross <rlpovey@hotmail.co.uk>
2020-12-14 20:42:10 +0000

    Added switch that removes holes -
    Should make this dynamic to activate function with same name of anything connected to it. I.e. spikes, holes etc.

---
+++ Entities/World/Switch.gd
@@ -0,0 +1,15 @@
+tool
+extends Area2D
+
+onready var connected_items: Array = get_tree().get_nodes_in_group('Switchable')
+
+func _ready():
+	pass
+	
+func _get_configuration_warning() -> String:
+	return "The switch has no connected items" if not connected_items else ""
+
+
+func _on_Switch_body_entered(body):
+	for item in connected_items:
+		item.queue_free()
