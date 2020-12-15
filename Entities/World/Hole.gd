1d4121008d6d13820222ab090bff15aaea0a8f79
latelearnerdev <latelearnerdev@hotmail.com>
2020-12-15 21:05:53 +0000

    Created constant and intermittent spikes

--- Entities/World/Hole.gd
+++ Entities/World/Hole.gd
@@ -1,5 +1,4 @@
 extends Area2D
 
-
 func _on_Hole_Entered(_body):
 	get_tree().reload_current_scene()
---
+++ Entities/World/Spikes.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+enum TYPE { INTERMITTENT, CONSTANT }
+export(TYPE) var type = TYPE.INTERMITTENT
+var animPlayer
+
+func _ready():
+	# This can easily be done with one animation player - just put all the sprites
+	# in the same file
+	if type == TYPE.INTERMITTENT:
+		print('INTERM')
+		$ApConstantSpikes.queue_free()
+		$ApConstantSpikes.queue_free()
+		animPlayer = $AnimationPlayer
+		animPlayer.play("Active")
+	elif type == TYPE.CONSTANT:
+		print('CONSTANT')
+		$AnimationPlayer.queue_free()
+		$Sprite.queue_free()
+		$CollisionShape2D.disabled = false
+		animPlayer = $ApConstantSpikes
+		animPlayer.play("ActiveConstant")
+		
+		
+
+func _on_Spikes_entered(_body):
+	print('spikes touched')
+	get_tree().reload_current_scene()
